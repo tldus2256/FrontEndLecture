f1ec67058cd1aff218071e9bf20a0778e6096642
siyeon2256 <siyeon198@gmail.com>
2024-01-08 15:30:43 +0900

    JS

---
+++ JavaScript/ch07.표준내장객체/04.String연습.js
@@ -0,0 +1,26 @@
+// 연습문제
+// 1. 1에서 1000사이에 0은 몇번, 1은 몇번, ...., 9는 몇번 사용되었는가
+//        123456789101112....9991000
+
+let numStr = '';
+for (let i = 1; i <= 1000; i++){
+    numStr += i;
+}
+
+// 1이 몇번 사용되었나?
+// 정규표현식 활용
+console.log(numStr.replace(/[^1]/g,'').length);
+
+for(let i = 0; i <= 9; i++){
+    let pattern = new RegExp('[^' + 1 + ']', 'g');
+    count = numStr.replace(pattern,'').length;
+    console.log(`${i}: ${count}`);
+}
+
+// count 배열을 만들어 일일이 체크
+let countArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+for(let i = 0; i < numStr.length; i++){
+    let num = Number(numStr[i]);
+    countArr[num]++;
+}
+console.log(countArr);
